46c9777ddeeab6b67230e8f0c8aaa021b9764f5d
geist <lysonnjoroge@gmail.com>
2020-03-21 19:23:48 +0300

    changes to lib_books

--- read_ip.py
+++ read_ip.py
@@ -3,15 +3,17 @@
 import numpy as np
 import pandas as pd
 #%%
-N_books = 0     # no of books
-l_libs = 0      # no of libraries
-d_days = 0      # no of days
-book_scores = None
-lib_stats = pd.DataFrame(columns=['noOfBooks', 'signUpTime', 'shipRate', 'books', 'totalScore']) # each lib stat = [N of books, signup time (I), ship rate (R), score of its books (bS)]
-# lib_books = []
 
 def read_ip(filename):
+    N_books = 0     # no of books
+    l_libs = 0      # no of libraries
+    d_days = 0      # no of days
+    book_scores = None
+    lib_stats = pd.DataFrame(columns=['noOfBooks', 'signUpTime', 'shipRate', 'books', 'totalScore']) # each lib stat = [N of books, signup time (I), ship rate (R), score of its books (bS)]
+    lib_books = None
+    
     with open(filename, 'r') as reader:
+
         lib_n_books = 0
 
         I_max, I_min = 0, 99999
@@ -49,6 +51,13 @@ def read_ip(filename):
             else:
                 # b0 b1 b2 ... bn
                 assert len(data) == lib_n_books
+                # still use lib_books
+                books = np.zeros(N_books, bool)
+                books[data] = True
+                if lib_books is None:
+                    lib_books = books
+                else:
+                    lib_books = np.vstack((lib_books, books))
                 books = tuple(data)
                 lib_stats.at[len(lib_stats)-1, 'books'] = books
                 # compute lib score
@@ -58,6 +67,10 @@ def read_ip(filename):
 
                 if bS > bS_max: bS_max = bS
                 if bS < bS_min: bS_min = bS
+    
+    bookCols = ['b%d'%i for i in range(N_books)]
+    books = pd.DataFrame(data=lib_books, columns=bookCols)
+    lib_stats = lib_stats.join(books)
 
     print('N_books:', N_books, 'l_libs:', l_libs, 'd_days:', d_days)
     print('book_scores:\n', book_scores)
@@ -65,12 +78,10 @@ def read_ip(filename):
     print(lib_stats)
     print('lib_books')
     print(lib_stats['books'])
-    return (book_scores, lib_stats)
+    return (book_scores, lib_stats, bookCols)
 
 #%%
 if __name__ == "__main__":
-    read_ip('a_example.txt')
-
-                
+    book_scores, lib_stats, bookCols = read_ip('a_example.txt')                
 
 # %%
---
+++ signup.py
@@ -0,0 +1,31 @@
+#%%
+import numpy as np
+import pandas as pd
+
+#%%
+def signup(libs, scores, libOrder):
+    scores = pd.to_numeric(scores['score']).to_numpy()
+    books = libs['books'].to_numpy()
+    print(books.dtype)
+    # libs['scores'] = scores['score'][libs['books']]
+    # print(libs['scores'])
+
+#%%
+def findLibQueue(book_scores, lib_stats, d_days):
+    books = np.array(list(lib_stats['books']))
+    print(books)
+    for i, lib in lib_stats.iterrows():
+        print(book_scores[books])
+    
+#%%
+from read_ip import read_ip
+if __name__ == "__main__":
+    book_scores, lib_stats, bookCols = read_ip('a_example.txt')
+
+#%%
+scores = np.array(book_scores['score'])
+print(scores)
+print(lib_stats['books'])
+# findLibQueue(scores, lib_stats, 7)
+
+# %%
